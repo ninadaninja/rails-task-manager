6523a9c694ec0bb83cbea88cdbd5d1002f08c93d
Nina Galindo <ninalorettagalindo@gmail.com>
2026-07-04 14:46:38 +0900

    user can now delete tasks

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "tasks", to: "tasks#create"
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
